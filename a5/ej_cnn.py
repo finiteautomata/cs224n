7713da4debaac754dc9a400b072ea45624f6ad54
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-08-19 09:37:55 -0300

    Add example of CNN

---
+++ a5/ej_cnn.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+if __name__ == "__main__":
+    "Ejemplo de CNN"
+    batch_len = 100
+    channels_in = 20
+    channels_out = 5
+    kernel_size = 7
+    seq_len = 10
+
+    # Nuestra entrada será de tamaño (100, 20, 10)
+    # Como el kernel size será de tamaño 7, el nuevo largo será 10 - 7 + 1 = 4
+    # Entonces, output será de tamaño (100, 5, 4)
+    inp = torch.rand(batch_len, channels_in, seq_len)
+    conv_layer = nn.Conv1d(channels_in, channels_out, kernel_size)
+
+    out = conv_layer(inp)
+
+    assert(out.shape == (batch_len, channels_out, 4))
+
+    print("Tests pasados correctamente")
